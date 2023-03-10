5b611c21eba3b68c90d064bb454e3e72b27c7a5c
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2023-03-10 04:22:25 -0300

    Upload

---
+++ index.js
@@ -0,0 +1,31 @@
+function insert(num) {
+    $("#result").append(num)
+    
+}
+
+function clearDisplay() {
+    $("#result").html('')
+}
+
+$(".backspace").click(function(){
+    var result = document.querySelector("#result").innerHTML    
+    document.querySelector("#result").innerHTML = result.substring(0, result.length - 1)
+})
+
+// var backspace = document.querySelector(".backspace")
+        
+// backspace.addEventListener("click", () => {
+    
+//         var result = document.querySelector("#result").innerHTML    
+//         document.querySelector("#result").innerHTML = result.substring(0, result.length - 1)
+//     })
+
+
+$(".btn-equal").click(function() {
+
+    var result = $("#result").html()
+
+    if(result) {
+        $("#result").html(eval(result))
+    }
+})
